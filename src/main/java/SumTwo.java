1f980e08ea2360ab814101f7ef83ea6878df6b67
eric yan <eric.yan828@gmail.com>
2020-02-19 23:16:34 -0500

    sum two - hash table

--- src/main/java/SumTwo.java
+++ src/main/java/SumTwo.java
@@ -1,10 +1,18 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * SumTwo
  */
 public class SumTwo {
-    public static int[] twoSum(int[] nums, int target) {
+    /**
+     * Time: O(n2), Space: O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] bruteForce(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
             for(int j = i+1; j < nums.length; j++) {
                 if(nums[i] + nums[j] == target) {
@@ -12,11 +20,31 @@ public class SumTwo {
                 }
             }
         }
-        return null;
+        throw new IllegalArgumentException("No solution");
+    }
+
+    /**
+     * Time: O(n), Space: O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] hashTable(int[] nums, int target) {
+        Map<Integer, Integer> valueIndexMap = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            valueIndexMap.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            Integer j = valueIndexMap.get(target - nums[i]);
+            if(j != null && j != i) {
+                return new int[] {i, j};
+            }
+        }
+        throw new IllegalArgumentException("No solution");
     }
 
     public static void main(String[] args) {
-        int[] rs = twoSum(new int[]{2, 7, 11, 15}, 9);
+        int[] rs = bruteForce(new int[]{2, 7, 11, 15}, 9);
         System.out.println(Arrays.toString(rs));
     }
 }
